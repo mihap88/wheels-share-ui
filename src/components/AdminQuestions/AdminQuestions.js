810f2c87f2012ce9adb6a84e97d7b6db5df9b08c
Alexandra Calota <allexa.calota@gmail.com>
2020-01-12 16:19:09 +0200

    Add exit icon to questions modal

--- src/components/AdminQuestions/AdminQuestions.js
+++ src/components/AdminQuestions/AdminQuestions.js
@@ -16,7 +16,10 @@ class AdminQuestions extends Component {
         return (
             <div className="modal display-block">
                 <div className="modal-main">
-                    <button onClick={this.props.closeModal} className="btn btn-primary">Submit</button>
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+                    <div className="topbar">
+                        <i onClick={this.props.closeModal} className="material-icons">clear</i>
+                    </div>
             </div>
             </div>
         )
