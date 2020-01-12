cc31e39849cafaa2a6848227ee8e923b0a96b782
Alexandra Calota <allexa.calota@gmail.com>
2020-01-12 03:53:57 +0200

    Many pages

---
+++ src/components/AdminInbox/AdminInbox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './AdminInbox.css';
+
+import {withRouter} from "react-router-dom";
+
+class AdminInbox extends Component {
+
+}
+
+
+export default withRouter(AdminInbox);
