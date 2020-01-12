7e41b2168cfc231fb711b081375d7831f4378e3f
Alexandra Calota <allexa.calota@gmail.com>
2020-01-12 21:12:34 +0200

    Navbar for user homepage

--- src/components/UserHomepage/UserHomepage.js
+++ src/components/UserHomepage/UserHomepage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './UserHomepage.css';
 import '../../common_css/UserNavbar.css';
 
@@ -13,16 +13,38 @@ class UserHomepage extends Component {
 
         }
     }
+
     componentDidMount() {
         console.log('user email: ' + this.state.email);
     }
 
     render() {
         return (
-            <div>
-                USER HOME PAGE
+        return (
+            <div className="Container">
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+                <script src="https://kit.fontawesome.com/yourcode.js"></script>
+                <div className="Navbar">
+                    <div className="Name">
+                        Wheels Share - Admin
+                    </div>
+                    <div className="AdminIcons">
+                        <div className="IconWrapper">
+                            <i onClick={this.handleNotifications} className="material-icons">notifications_active</i>
+                            <span className="badge">{this.state.notifications_badge}</span>
+                        </div>
+                        <div className="IconWrapper">
+                            <i onClick={this.handleQuestionsModal} className="material-icons">email</i>
+                            <span className="badge">{this.state.questions_badge}</span>
+                        </div>
+                        <div className="IconWrapper">
+                            <i onClick={() => {
+                                this.props.history.push('/');
+                            }} className="material-icons">exit_to_app</i>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         );
     }
 }
