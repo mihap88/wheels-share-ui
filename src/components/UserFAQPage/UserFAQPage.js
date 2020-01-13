0a2b1e194b9ec6a7a7ae415c4e1247d00422bf97
Alex Graure <alexgraure16@gmail.com>
2020-01-14 00:14:59 +0200

    Add FAQ implementation

--- src/components/UserFAQPage/UserFAQPage.js
+++ src/components/UserFAQPage/UserFAQPage.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import './UserFAQPage.css';
 import '../../common_css/UserNavbar.css';
 import '../../common_css/UserSidebar.css';
+import axios from "axios";
+import {QUESTIONS_SERVICE} from "../../App";
 
 import {withRouter} from "react-router-dom";
 
@@ -12,6 +14,7 @@ class UserFAQPage extends Component {
         this.state = {
             email: this.props.location.state.email,
             questions: [],
+            newQuestion: ''
         }
     }
 
@@ -25,10 +28,46 @@ class UserFAQPage extends Component {
         })
     }
 
+    handleNewQuestion = (q) => {
+        this.setState({
+            newQuestion: q.target.value
+        })
+    };
+
+    sendAnswer = (q) => {
+        const request_url = QUESTIONS_SERVICE + "/questions/add";
+
+        const postData = {
+            user_email_address: this.state.email,
+            question: this.state.newQuestion,
+        };
+
+        axios.post(request_url, postData, {timeout: 10000})
+            .then((response) => {
+                if (response.data === 'OK') {
+                    
+                    // let input = document.getElementById("answer");
+                    // input.value = "";
+                }
+            });
+    }
+
+
+
     displayQuestion = (question_data) => {
         return (
-            <div>
-                question_data.question
+            <div className="question-block-user">
+                <div className="question-user">
+                    <div className="email-user">
+                        {question_data.userEmailAddress}
+                    </div>
+                    <div className="question-text-user">
+                        {question_data.question}
+                    </div>
+                </div>
+                <div className="answer-user">
+                    {question_data.answer}
+                </div>
             </div>
         );
     }
@@ -43,9 +82,41 @@ class UserFAQPage extends Component {
 
         return (
             <div className="page display-block">
-                {this.state.questions.map((question_data, id) => {
-                    return this.displayQuestion(question_data)
-                })}
+                {/* <div className="add-question-block">
+                    <input type="submit" value="Add question" required></input>
+                </div> */}
+
+                {/* <div className="add-question-block">
+                    <input onChange={this.handleNewQuestion} id="question"
+                           className="question-input form-group mx-sm-3 mb-2 from-control"
+                           placeholder="Ask a new question here"/>
+                    <button onClick={this.sendAnswer} id={10} className="btn btn-primary mb-2">
+                        Add question
+                    </button>
+                </div> */}
+                <div className="add-question-block">
+                    <div >
+                        <button onClick={this.sendAnswer} id={10} className="btn btn-primary mb-2">
+                            Add question
+                        </button>
+                    </div>
+
+                    <div >
+                        <textarea 
+                            onChange={this.handleNewQuestion} 
+                            className="new-question-textarea" 
+                            aria-label="With textarea"
+                            rows="3">
+                        </textarea>
+                    </div>
+                    
+                </div>
+                
+                <div className="modal-content-question-user">
+                    {this.state.questions.map((question_data, id) => {
+                        return this.displayQuestion(question_data)
+                    })}
+                </div>
             </div>
         )
     }
